36f3e5895aa61d0a2e9cff2529cb9df73d906931
Josh Hulsey <jhul0388@gmail.com>
2017-08-02 08:58:04 -0500

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,6 +31,8 @@ function nutSearch() {
 
   connect.query("select * from products", function(err, res) {
 
+//This for loop is suppose to look through the products to display for purchase.
+
     for (var i = 0; i < res.length; i++) {
       console.log("ID: " + res[i].item_id);
       console.log("Product: " + res[i].product_name);
@@ -41,3 +43,10 @@ function nutSearch() {
     }
   });
 }
+
+//Once a selection is made  I will need to display Quantity and price.
+// They will need to select the amount they want to purcahse.
+//If the store has enough the purchase will proceed. If not it will error out that there is not enough stock.
+//Afterwards I will need to have a function if a item is selected to deduct from the available list.
+//The Quantity will need to update after a purchase is made.
+// The start screen will then re-appear to make another purchase.
